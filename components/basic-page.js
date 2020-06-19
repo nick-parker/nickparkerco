9456a71972fb2abe5524e5b733d566ba2119b248
Nick Parker <nmp67@cornell.edu>
2020-06-18 20:09:42 -0700

    Lightly improved styling (still bad), added pics

--- components/basic-page.js
+++ components/basic-page.js
@@ -5,7 +5,7 @@ export default function BasicPage({title, children}) {
         <>
             <Nav/>
             <h1 className="title">{title}</h1>
-            <div className="text-left sm:text-lg md:text-xl w-3/4 mx-auto">
+            <div className="text-left sm:text-base md:text-lg w-3/4 mx-auto">
                 {children}
             </div>
         </>
